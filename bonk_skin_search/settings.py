ae513ce88c09b2449cd361eed2645f84d0c6abd2
Jorge Urias <jurias@andrew.cmu.edu>
2025-09-08 02:03:42 -0400

    Fixing Changelog server errors and making them globally available

--- bonk_skin_search/settings.py
+++ bonk_skin_search/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                "skins.context_processors.latest_update",
+                'skins.context_processors.latest_update',
             ],
         },
     },
