fe0746a80c319047633b415c842b822f83545ba8
Jorge Urias <jurias@andrew.cmu.edu>
2025-09-15 00:18:08 -0400

    Refetching all skin codes for each skin

--- skins/management/commands/backfill_skin_codes.py
+++ skins/management/commands/backfill_skin_codes.py
@@ -1,14 +1,30 @@
+# skins/management/commands/backfill_skin_codes.py
 import re
 import requests
 from django.core.management.base import BaseCommand
 from skins.models import Skin
-from skins.utils.skin_code import extract_skin_code
+from urllib.parse import urlparse, parse_qs, unquote
 
 # Regex to capture the src attribute from <img id="sharedimg">
 IMG_RE = re.compile(
     r'<img[^>]+id=["\']sharedimg["\'][^>]+src=["\']([^"\']+)["\']',
     re.IGNORECASE,
 )
+
+def extract_skin_code(link_or_img_url: str) -> str | None:
+    """
+    Extract the skinCode query parameter from a Bonkleagues shared image URL.
+    Example: https://sebweb.co.uk/blbackend/prevgen.php?...&skinCode=XYZ
+    """
+    if not link_or_img_url or "skinCode=" not in link_or_img_url:
+        return None
+    parsed_url = urlparse(link_or_img_url)
+    query = parse_qs(parsed_url.query)
+    raw_code = query.get("skinCode", [None])[0]
+    if raw_code:
+        return unquote(raw_code)  # decode %2F, etc.
+    return None
+
 
 class Command(BaseCommand):
     help = "Backfill the skin_code column for all skins by scraping Bonkleagues links"
@@ -32,7 +48,7 @@ class Command(BaseCommand):
 
                 img_src = match.group(1)
 
-                # --- Extract skinCode using util ---
+                # --- Extract skinCode directly ---
                 code = extract_skin_code(img_src)
                 if code:
                     skin.skin_code = code
